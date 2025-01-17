cf82361f90883030cc116aa6367706da2a86079e
Jakub Mikians <2897102+jakub-m@users.noreply.github.com>
2025-01-17 09:35:42 +0100

    Add <UTC>

--- scripts/generate_tests_cases.py
+++ scripts/generate_tests_cases.py
@@ -6,6 +6,8 @@ def main():
     for d in iter_deltas():
         t = t0 + d
         print(f"{t!r}\t{isoformat_z(t)}")
+    for s in iter_datetime_str():
+        print(f"{s}")
 
 
 def isoformat_z(d: datetime) -> str:
@@ -17,5 +19,7 @@ def iter_deltas():
     yield timedelta(milliseconds=1)
     yield timedelta(milliseconds=-1)
 
+def iter_datetime_str():
+    yield 'datetime.datetime(2024, 8, 13, 16, 11, tzinfo=<UTC>)'
 
 main()
